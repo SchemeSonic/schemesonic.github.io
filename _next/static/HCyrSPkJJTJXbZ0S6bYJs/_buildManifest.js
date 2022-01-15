9c326a8e71cb223a6edcd93aac9f2764671bd61f
SchemeSonic <SchemeSonic@users.noreply.github.com>
2022-01-15 21:09:51 +0000

    deploy: 4eb1755aa5fe5f56f6e83ec76eea3ded0811787f

--- _next/static/HCyrSPkJJTJXbZ0S6bYJs/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b6074a566de8c43d.css","static/chunks/pages/index-fc287bf93df8f850.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
